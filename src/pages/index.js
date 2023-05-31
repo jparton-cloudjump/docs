1ada127984a81dfb69742e5efda391acf6248bf5
jkitchens32 <jennifer.kitchens@enterprisedb.com>
2023-05-31 08:57:54 -0400

    Added index

--- src/pages/index.js
+++ src/pages/index.js
@@ -360,6 +360,8 @@ const Page = () => (
             <IndexCardLink to="/partner_docs/EsriArcGISProandEsriArcGISEnterprise">
               Esri ArcGIS Pro and Esri ArcGIS Enterprise
             </IndexCardLink>
+            <IndexCardLink to="/partner_docs/HPEGreenlake">
+              HPE Greenlake
             <IndexCardLink to="/partner_docs/ImpervaDataSecurityFabric">
               Imperva Data Security Fabric
             </IndexCardLink>
